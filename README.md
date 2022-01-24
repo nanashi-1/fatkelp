51551369bbdc6d8ed5ca6d8e02d946545bd50f6b
nanashi <bagaresalken@gmail.com>
2022-01-24 09:39:23 +0800

    added in v1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# fatkelp
+
+`fatkelp` is a program that forwards events from /dev/input/eventX to /dev/uinput and also remap keys
+
+## Usage
--- installer.sh
+++ installer.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+git clone https://github.com/algames2019/fatkelp.git
+cd fatkelp
+
+cp fatkelp /usr/bin
+cp global.fk /usr/share/fatkelp
